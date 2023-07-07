aefd00c0ce489f59b44b9316e86afeec1d98cca1
Wenyu Zheng <zhengwy229@gmail.com>
2023-07-07 22:25:32 +0100

    Added style

--- src/components/Drum.js
+++ src/components/Drum.js
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import audios from "../constants/audios";
 import DrumPad from "./DrumPad";
+import "./css/Drum.css";
 
 const Drum = () => {
   const [audioName, setAudioName] = useState(null);
 
   return (
     <div id="drum-machine">
-      {audios.map((e) => (
-        <DrumPad key={e.name} audio={e} setAudioName={setAudioName} />
-      ))}
+      <div id="drum-pad-box">
+        {audios.map((e) => (
+          <DrumPad key={e.name} audio={e} setAudioName={setAudioName} />
+        ))}
+      </div>
       <div id="display">{audioName}</div>
     </div>
   );
--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import "./css/DrumPad.css";
 
 const DrumPad = ({ audio, setAudioName }) => {
   const sound = new Audio(audio.src);
 
+  const [clicked, setClicked] = useState(false);
+
   const handleClick = () => {
+    setClicked(true);
     setAudioName(audio.name);
     sound.play();
+    setTimeout(() => setClicked(false), 100);
   };
 
   return (
-    <div className="drum-pad" onClick={handleClick}>
+    <div
+      className="drum-pad"
+      onClick={handleClick}
+      style={{ backgroundColor: clicked ? "orange" : "rgb(168, 167, 167)" }}
+    >
       {audio.key}
     </div>
   );
