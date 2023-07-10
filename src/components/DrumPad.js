4431d4f1e313218e19915c29da8bf9dc1b28396e
Wenyu Zheng <zhengwy229@gmail.com>
2023-07-10 23:53:47 +0100

    pass 7/8 of tests

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -2,25 +2,28 @@ import { useState } from "react";
 import "./css/DrumPad.css";
 
 const DrumPad = ({ audio, setAudioName, volume }) => {
-  const sound = new Audio(audio.src);
-
   const [clicked, setClicked] = useState(false);
 
   const handleClick = () => {
+    const sound = document.getElementById(audio.key);
+
     sound.volume = volume / 100;
     setClicked(true);
     setAudioName(audio.name);
     sound.play();
+
     setTimeout(() => setClicked(false), 100);
   };
 
   return (
     <div
+      id={audio.name}
       className="drum-pad"
       onClick={handleClick}
       style={{ backgroundColor: clicked ? "orange" : "rgb(168, 167, 167)" }}
     >
       {audio.key}
+      <audio className="clip" id={audio.key} src={audio.src} />
     </div>
   );
 };
