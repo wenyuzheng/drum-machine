6cbfa09806febe1ac82ad18072d21e3f9eaafef8
Wenyu Zheng <zhengwy229@gmail.com>
2023-07-11 11:42:02 +0100

    Added handleKeyPressed

--- src/components/Drum.js
+++ src/components/Drum.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import audios from "../constants/audios";
 import DrumPad from "./DrumPad";
 import "./css/Drum.css";
@@ -8,13 +8,25 @@ const Drum = () => {
   const [audioName, setAudioName] = useState(null);
   const [volume, setVolume] = useState(50);
 
+  const handleKeyPressed = (e) => {
+    const aud = audios.find((k) => k.key === e.key.toUpperCase());
+    if (aud) {
+      document.getElementById(aud.key).click();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyPressed);
+    return () => window.removeEventListener("keydown", handleKeyPressed);
+  }, []);
+
   return (
     <div id="drum-machine">
       <div id="drum-pad-box">
         {audios.map((e) => (
           <DrumPad
             id={e.key}
-            key={e.name}
+            key={e.key}
             audio={e}
             setAudioName={setAudioName}
             volume={volume}
