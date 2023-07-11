795346c55f2b377f4a791822a22f8011422edb2e
Wenyu Zheng <zhengwy229@gmail.com>
2023-07-11 12:22:43 +0100

    Removed clicked state

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,27 +1,19 @@
-import { useState } from "react";
 import "./css/DrumPad.css";
 
 const DrumPad = ({ audio, setAudioName, volume }) => {
-  const [clicked, setClicked] = useState(false);
-
   const handleClick = () => {
     const sound = document.getElementById(audio.key);
-
     sound.volume = volume / 100;
-    setClicked(true);
     setAudioName(audio.name);
     sound.play();
 
-    setTimeout(() => setClicked(false), 100);
+    const pad = document.getElementById(audio.name);
+    pad.style.backgroundColor = "orange";
+    setTimeout(() => (pad.style.backgroundColor = null), 100);
   };
 
   return (
-    <div
-      id={audio.name}
-      className="drum-pad"
-      onClick={handleClick}
-      style={{ backgroundColor: clicked ? "orange" : "rgb(168, 167, 167)" }}
-    >
+    <div id={audio.name} className="drum-pad" onClick={handleClick}>
       {audio.key}
       <audio className="clip" id={audio.key} src={audio.src} />
     </div>
